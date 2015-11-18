a49c2d845941b512c44f69fab9a8373c1f1e2c14
LovingYoung <ly950710@gmail.com>
2015-11-18 21:03:32 +0800

    Accomplish to build parse tree

--- Syntactic.py
+++ Syntactic.py
@@ -1,36 +1,44 @@
 class PROG:
     @staticmethod
     def process(instr):
-        a1 = PROGHEAD.process(instr)
-        a2 = OTHER.process(instr)
-        return a1 and a2
+        root = TreeNode(typename='PROGRAM')
+        a1 = PROGHEAD.process(instr, root)
+        a2 = BLOCK.process(instr, root)
+        return (a1 and a2, root)
 
 
 class PROGHEAD:
     @staticmethod
-    def process(instr):
+    def process(instr, root):
+        myroot = TreeNode(parent=root, typename='Program_Head')
         a1 = (instr[0].getTypeId() == 1)
-        a2 = (instr[1].getTypeId() >= 1000 and instr[0].getTypeId() < 100000)
-        if a1 and a2:
-            instr.pop(0)
-            instr.pop(0)
-            return True
-        else:
+        if not a1:
             return False
+        son = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
+        instr.pop(0)
+        a2 = (instr[0].getTypeId() == 1000)
+        if not a2:
+            return False
+        son = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
+        instr.pop(0)
+        return True
 
 
-class OTHER:
+class BLOCK:
     @staticmethod
     def getfirst():
         return [4, 5, 2, 1000, 8, 6]
 
     @staticmethod
-    def process(instr):
+    def process(instr,root):
+        myroot = TreeNode(parent=root, typename='Block')
         if instr[0].getTypeId() in CONSTINFO.getfirst():
-            if not CONSTINFO.process(instr): return False
+            if not CONSTINFO.process(instr, myroot):
+                return False
         if instr[0].getTypeId() in PARAINFO.getfirst():
-            if not PARAINFO.process(instr): return False
-        return SENTENCEPART.process(instr)
+            if not PARAINFO.process(instr, myroot):
+                return False
+        return SENTENCEPART.process(instr, myroot)
 
 
 class CONSTINFO:
@@ -39,13 +47,15 @@ class CONSTINFO:
         return [4]
 
     @staticmethod
-    def process(instr):
+    def process(instr, root):
+        myroot = TreeNode(parent=root,typename='Const_Statement')
         a1 = (instr[0].getTypeId() == 4)
         if a1:
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
-            a2 = CONSTDEF.process(instr)
+            a2 = CONSTDEF.process(instr,myroot)
             if a2:
-                while CONSTDEF.process(instr):
+                while CONSTDEF.process(instr,myroot):
                     pass
                 return True
             else:
@@ -58,19 +68,23 @@ class CONSTDEF:
         return [1000]
 
     @staticmethod
-    def process(instr):
+    def process(instr, root):
+        myroot = TreeNode(parent=root, typename='Const Definition')
         a1 = (instr[0].getTypeId() == 1000)
-        if a1 == True:
+        if a1:
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
         else:
             return False
         a2 = (instr[0].getTypeId() == 305)
         if a2 == True:
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
         else:
             return False
         a3 = (instr[0].getTypeId() == 200)
         if a3 == True:
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
         else:
             return False
@@ -83,20 +97,24 @@ class PARAINFO:
         return [5]
 
     @staticmethod
-    def process(instr):
+    def process(instr, root):
+        myroot = TreeNode(parent=root,typename='Parameter_Statement')
         a1 = (instr[0].getTypeId() == 5)
         if a1:
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
         else:
             return False
 
         a2 = (instr[0].getTypeId() == 1000)
         if a2 == True:
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
         else:
             return False
 
         while(instr[0].getTypeId() == 1000):
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
 
         return True
@@ -108,11 +126,12 @@ class SENTENCEPART:
         return [2, 1000, 8, 6]
 
     @staticmethod
-    def process(instr):
+    def process(instr,root):
+        myroot = TreeNode(parent=root, typename='Sentence_Part')
         if instr[0].getTypeId() in SENTENCE.getfirst():
-            return SENTENCE.process(instr)
+            return SENTENCE.process(instr, myroot)
         elif instr[0].getTypeId() in COMPLEXSENTENCE.getfirst():
-            return COMPLEXSENTENCE.process(instr)
+            return COMPLEXSENTENCE.process(instr, myroot)
         return False
 
 
@@ -122,23 +141,27 @@ class COMPLEXSENTENCE:
         return [2]
 
     @staticmethod
-    def process(instr):
+    def process(instr, root):
+        myroot = TreeNode(parent=root,typename='Complex_Sentences')
         a1 = (instr[0].getTypeId() == 2)
         if not a1:
             return False
+        ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
         instr.pop(0)
 
-        a2 = SENTENCE.process(instr)
+        a2 = SENTENCE.process(instr,myroot)
         if not a2:
             return False
 
-        while(SENTENCE.process(instr) == True): pass
+        while SENTENCE.process(instr, myroot): pass
 
         a3 = (instr[0].getTypeId() == 3)
-        if a3 == False:
+        ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
+        instr.pop(0)
+        if not a3:
             return False
 
-        return a1 and a2 and a3
+        return True
 
 
 class SENTENCE:
@@ -147,18 +170,19 @@ class SENTENCE:
         return [2, 1000, 8 ,6]
 
     @staticmethod
-    def process(instr):
+    def process(instr, root):
+        myroot = TreeNode(parent=root, typename='Sentence')
         if instr[0].getTypeId() in ASSIGNMENT.getfirst():
-            return ASSIGNMENT.process(instr)
+            return ASSIGNMENT.process(instr, myroot)
 
         if instr[0].getTypeId() in CONDITIONSENTENCE.getfirst():
-            return CONDITIONSENTENCE.process(instr)
+            return CONDITIONSENTENCE.process(instr, myroot)
 
         if instr[0].getTypeId() in LOOP.getfirst():
-            return LOOP.process(instr)
+            return LOOP.process(instr, myroot)
 
         if instr[0].getTypeId() in COMPLEXSENTENCE.getfirst():
-            return COMPLEXSENTENCE.process(instr)
+            return COMPLEXSENTENCE.process(instr, myroot)
 
         return False
 
@@ -169,18 +193,21 @@ class ASSIGNMENT:
         return [1000]
 
     @staticmethod
-    def process(instr):
+    def process(instr, root):
+        myroot = TreeNode(parent=root, typename='Assignment')
         a1 = (instr[0].getTypeId() == 1000)
-        if a1 == False:
+        if not a1:
             return False
+        ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
         instr.pop(0)
 
         a2 = (instr[0].getTypeId() == 305)
-        if a2 == False:
+        if not a2:
             return False
+        ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
         instr.pop(0)
 
-        return EXPRESSION.process(instr)
+        return EXPRESSION.process(instr, myroot)
 
 
 class EXPRESSION:
@@ -189,15 +216,16 @@ class EXPRESSION:
         return [301, 302, 1000, 200, 400]
 
     @staticmethod
-    def process(instr):
-        a1 = PLUSOP.process(instr)
-        a2 = TERM.process(instr)
-        if a2 == False:
+    def process(instr, root):
+        myroot = TreeNode(parent=root, typename='Expression')
+        a1 = PLUSOP.process(instr, myroot)
+        a2 = TERM.process(instr, myroot)
+        if not a2:
             return False
         temp = list(instr)
-        while(PLUSOP.process(temp) and TERM.process(temp)):
-            PLUSOP.process(instr)
-            TERM.process(instr)
+        while(PLUSOP.process(temp, myroot) and TERM.process(temp, myroot)):
+            PLUSOP.process(instr, myroot)
+            TERM.process(instr, myroot)
         return True
 
 
@@ -207,14 +235,15 @@ class TERM:
         return [1000, 200, 400]
 
     @staticmethod
-    def process(instr):
-        a1 = FACTOR.process(instr)
-        if a1 == False:
+    def process(instr, root):
+        myroot = TreeNode(parent=root, typename='Term')
+        a1 = FACTOR.process(instr, myroot)
+        if not a1:
             return False
         temp = list(instr)
-        while(MULTIOP.process(temp) and TERM.process(temp)):
-            MULTIOP.process(instr)
-            TERM.process(instr)
+        while(MULTIOP.process(temp, None) and TERM.process(temp, None)):
+            MULTIOP.process(instr, myroot)
+            TERM.process(instr, myroot)
         return True
 
 
@@ -224,12 +253,15 @@ class FACTOR:
         return [1000, 200, 400]
 
     @staticmethod
-    def process(instr):
+    def process(instr, root):
+        myroot = TreeNode(parent=root, typename='Factor')
         if instr[0].getTypeId() == 1000:
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
             return True
 
         if instr[0].getTypeId() == 200:
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
             return True
 
@@ -237,10 +269,12 @@ class FACTOR:
         a1 = (temp[0].getTypeId() == 400)
         if a1:
             temp.pop(0)
-            if EXPRESSION.process(temp):
+            if EXPRESSION.process(temp, None):
                 if temp[0].getTypeId() == 401:
+                    ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
                     instr.pop(0)
-                    EXPRESSION.process(instr)
+                    EXPRESSION.process(instr, myroot)
+                    ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
                     instr.pop(0)
                     return True
         return False
@@ -252,11 +286,14 @@ class PLUSOP:
         return [301, 302]
 
     @staticmethod
-    def process(instr):
+    def process(instr, root):
+        myroot = TreeNode(parent=root, typename='Plus_Operator')
         if instr[0].getTypeId() == 301:
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
             return True
         if instr[0].getTypeId() == 302:
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
             return True
         return False
@@ -268,11 +305,14 @@ class MULTIOP:
         return [303, 304]
 
     @staticmethod
-    def process(instr):
+    def process(instr, root):
+        myroot = TreeNode(parent=root, typename='Multiply_Operator')
         if instr[0].getTypeId() == 303:
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
             return True
         if instr[0].getTypeId() == 304:
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
             return True
 
@@ -283,13 +323,17 @@ class CONDITIONSENTENCE:
         return [8]
 
     @staticmethod
-    def process(instr):
+    def process(instr, root):
+        myroot = TreeNode(parent=root, typename='Condition_Sentence')
         if instr[0].getTypeId() == 8:
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
-            CONDITION.process(instr)
+            CONDITION.process(instr, myroot)
             if instr[0].getTypeId() == 9:
+                ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
                 instr.pop(0)
-                if SENTENCE.process(instr): return True
+                if SENTENCE.process(instr, myroot):
+                    return True
         return False
 
 
@@ -299,13 +343,17 @@ class LOOP:
         return [6]
 
     @staticmethod
-    def process(instr):
+    def process(instr, root):
+        myroot = TreeNode(parent=root, typename='Loop_Sentence')
         if instr[0].getTypeId() == 6:
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
-            CONDITION.process(instr)
+            CONDITION.process(instr, myroot)
             if instr[0].getTypeId() == 7:
+                ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
                 instr.pop(0)
-                if SENTENCE.process(instr) == True: return True
+                if SENTENCE.process(instr, myroot):
+                    return True
         return False
 
 
@@ -315,10 +363,11 @@ class CONDITION:
         return [301, 302, 1000, 200, 400]
 
     @staticmethod
-    def process(instr):
-        if EXPRESSION.process(instr):
-            if RELATIONOP.process(instr):
-                if EXPRESSION.process(instr):
+    def process(instr, root):
+        myroot = TreeNode(parent=root, typename='Condition_Sentence')
+        if EXPRESSION.process(instr, myroot):
+            if RELATIONOP.process(instr, myroot):
+                if EXPRESSION.process(instr, myroot):
                     return True
         return False
 
@@ -329,8 +378,10 @@ class RELATIONOP:
         return [306, 307, 308, 309, 310, 311]
 
     @staticmethod
-    def process(instr):
+    def process(instr, root):
+        myroot = TreeNode(parent=root, typename='Relationship_Sentence')
         if instr[0].getTypeId() in RELATIONOP.getfirst():
+            ter = TreeNode(parent=myroot, typename='Terminator', value=instr[0])
             instr.pop(0)
             return True
         else:
@@ -338,9 +389,11 @@ class RELATIONOP:
 
 
 class TreeNode:
-    def __init__(self, children=[], parent=None, typename=None, value=None):
-        self._children = children
+    def __init__(self, parent=None, typename=None, value=None):
+        self._children = list([])
         self._parent = parent
+        if parent != None:
+            parent.addChildren(self)
         self._type = typename
         self._value = value
 
@@ -357,11 +410,14 @@ class TreeNode:
         return self._value
 
     def addChildren(self, children):
-        self._children.append(children)
+        if children not in self._children:
+            self._children.append(children)
         return self._children
 
     def setParent(self, parent):
         self._parent = parent
+        if parent != None:
+            parent.addChildren(self)
         return self._parent
 
     def setType(self, typename):
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import LexAnalysis
 import Syntactic
 
+def printParseTree(root, level = 0):
+    children = root.getChildren()
+    if root.getType() == 'Terminator':
+        print(level * 2 * ' ' + 'Type:' + 'Terminator' + ' Value:' + root.getValue().getValue())
+    else:
+        print(level * 2 * ' ' + 'Type:' + root.getType())
+
+    for i in root.getChildren():
+        printParseTree(i, level + 1)
+
 a = LexAnalysis.LexAnalysis()
 a.readFromFile("testLex")
 ans = a.lex()
-print(Syntactic.PROG.process(ans))
+processAns = Syntactic.PROG.process(ans)
+parseTreeRoot = processAns[1]
+print(processAns[0])
+printParseTree(parseTreeRoot)
