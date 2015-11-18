16593a9be26d73df9541462b93fee5a1c2579a1c
LovingYoung <ly950710@gmail.com>
2015-11-18 22:21:54 +0800

    Optimize User Interface

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 import LexAnalysis
 import Syntactic
 
-def printParseTree(root, level = 0):
-    children = root.getChildren()
-    if root.getType() == 'Terminator':
-        print(level * 2 * ' ' + 'Type:' + 'Terminator' + ' Value:' + root.getValue().getValue())
+def printParseTree(root, level = 1, parent = -1, up = list(), lastChild = False):
+    if parent == -1:
+        prefix = '└── '
     else:
-        print(level * 2 * ' ' + 'Type:' + root.getType())
+        if parent not in up:
+            up.append(parent)
+        prefix = list(4 * level * ' ')
+        for i in up:
+            prefix[i] = '│'
+        if lastChild:
+            prefix[parent] = '└'
+            up.remove(parent)
+        else:
+            prefix[parent] = '├'
+        i = parent + 1
+        while i < len(prefix) - 1:
+            prefix[i] = '─'
+            i += 1
+        i = len(prefix) - 1
+        prefix[i] = ' '
+        prefix = ''.join(prefix)
 
-    for i in root.getChildren():
-        printParseTree(i, level + 1)
+    if root.getType() == 'Terminator':
+        print(prefix + 'Type:' + 'Terminator' + ' Value:' + root.getValue().getValue())
+    else:
+        print(prefix + 'Type:' + root.getType())
+
+    l = root.getChildren()
+    i = 0
+    if len(l) > 0:
+        while i < len(l) - 1:
+            printParseTree(l[i], level + 1, parent=level * 4, up=up, lastChild=False)
+            i += 1
+        printParseTree(l[len(l) - 1], level + 1, parent=level * 4, up=up, lastChild=True)
 
 a = LexAnalysis.LexAnalysis()
 a.readFromFile("testLex")
